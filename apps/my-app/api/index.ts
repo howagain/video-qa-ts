db4f189a73d7a9465940235aeb6338c9fd3a4d56
Jacob Haugen <howagain@gmail.com>
2025-05-10 12:14:19 -0600

    trying to remove edge runtime

--- apps/my-app/api/index.ts
+++ apps/my-app/api/index.ts
@@ -1,10 +1,6 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
-export const config = {
-  runtime: 'edge'
-}
-
 const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
